598309ef8a3854f23c5e8f8b625c4a61269f1fa1
Chris Bradfield <cb@scribe.net>
2019-03-19 13:02:40 -0700

    Added coins

--- vths_jump.py
+++ vths_jump.py
@@ -16,6 +16,17 @@ mario_img = pygame.image.load("Downloads/mario_right.png")
 mario_img = pygame.transform.rotozoom(mario_img, 0, 0.2)
 mario_left = pygame.transform.flip(mario_img, True, False)
 block_img = pygame.image.load("Downloads/grass_main_32x32.png")
+# to resize
+block_img = pygame.transform.scale(block_img, (32, 32))
+coin_img = pygame.image.load("Downloads/coin.png")
+coin_img = pygame.transform.scale(coin_img, (30, 30))
+
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__(all_sprites, coins)
+        self.image = coin_img
+        self.rect = self.image.get_rect(topleft=(x, y))
+        
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__(all_sprites)
@@ -54,7 +65,7 @@ class Player(pygame.sprite.Sprite):
 class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__(all_sprites, platforms)
-        self.image = block_img#pygame.Surface((32, 32))
+        self.image = block_img #pygame.Surface((32, 32))
         #self.image.fill( (14, 183, 63) )
         self.rect = self.image.get_rect(topleft=(x, y))
 
@@ -73,9 +84,8 @@ class Camera:
 
 all_sprites = pygame.sprite.Group()
 platforms = pygame.sprite.Group()
-# for x in range(5, 20):
-#     Platform(x*32, 550)
-# player = Player(WIDTH/2, 400)
+coins = pygame.sprite.Group()
+
 level = []
 with open("level1.txt") as f:
     for line in f:
@@ -86,6 +96,8 @@ for row, items in enumerate(level):
             Platform(col*32, row*32)
         if item == "$":
             player = Player(col*32, row*32)
+        if item == "o":
+            Coin(col*32, row*32)
 camera = Camera(len(level[0])*32, len(level)*32)
 
 running = True
